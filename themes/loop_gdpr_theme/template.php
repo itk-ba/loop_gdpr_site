10e21293dfd29e674553810069fdefc981dc083a
Martin Yde Granath <my@aarhus.dk>
2018-06-20 15:31:26 +0200

    SALG-347: Added styling

--- themes/loop_gdpr_theme/template.php
+++ themes/loop_gdpr_theme/template.php
@@ -10,3 +10,15 @@ function loop_gdpr_theme_preprocess_html(&$vars) {
     'preprocess' => FALSE,
   ));
 }
+
+/**
+ * Override or insert variables into the page template.
+ */
+function loop_gdpr_theme_preprocess_page(&$variables) {
+  if (user_is_logged_in()) {
+    // Apply search links if module is active.
+    if (module_exists('loop_search_links')) {
+      $variables['search_links'] = module_invoke('loop_search_links', 'block_view', 'loop_search_links_block');
+    }
+  }
+}
